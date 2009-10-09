e932245a8a359e6a6a499cd43e311fa4cf8918ee
jonathan@abbett.org <jonathan@abbett.org@f1e69594-b2ab-11de-ae6a-cd50274c347e>
2009-10-09 18:20:33 +0000

    Added e-mail support. Added session support.

--- trunk/src/main/resources/clickframes/php/autoscan/webapp/application/controllers/${appspec.id}/${page.id}.php
+++ trunk/src/main/resources/clickframes/php/autoscan/webapp/application/controllers/${appspec.id}/${page.id}.php
@@ -1,25 +1,44 @@
 <?php
 
-	$data['applicationTitle'] = $this->lang->line('${appspec.id}_title');;
-	$data['pageTitle'] = $this->lang->line('${appspec.id}_${page.id}_title');
-	$data['pageId'] = '${page.id}';
+/*
+**	${page.title}
+**	${page.description}
+**
+#foreach ($fact in $page.facts)**	- ${fact.description}
+#end
+*/
 
-	$data['message'] = $this->session->flashdata('message');
+#if ($page.loginRequired)
+if (!$this->clickframes->sessionAlive()) {
+	$this->session->set_flashdata('referer', uri_string());
+	redirect('/${appspec.id.toLowerCase()}/${appspec.security.defaultLoginPage.id}');
+}
+#end
 
-	## navigation
-	$data['navigation'] = '';
-	#foreach ($linkSet in $appspec.globalLinkSets)
-	$data['navigation'] .= $this->load->view('navigation/${linkSet.id.toLowerCase()}', '', true);
-	#end
-	#foreach ($linkSet in $page.linkSets)
-	$data['navigation'] .= $this->load->view('navigation/${linkSet.id.toLowerCase()}', '', true);
-	#end
+#if ($page.loginPage)
+$this->session->keep_flashdata('referer');
+#end
 
-	## header view
-	$this->load->view('header', $data);
-	## this view
-	$this->load->view('${page.id.toLowerCase()}', $data);
-	## footer view
-	$this->load->view('footer', $data);
+$data['applicationTitle'] = $this->lang->line('${appspec.id}_title');;
+$data['pageTitle'] = $this->lang->line('${appspec.id}_${page.id}_title');
+$data['pageId'] = '${page.id}';
+
+$data['message'] = $this->session->flashdata('message');
+
+## navigation
+$data['navigation'] = '';
+#foreach ($linkSet in $appspec.globalLinkSets)
+$data['navigation'] .= $this->load->view('navigation/${linkSet.id.toLowerCase()}', '', true);
+#end
+#foreach ($linkSet in $page.linkSets)
+$data['navigation'] .= $this->load->view('navigation/${linkSet.id.toLowerCase()}', '', true);
+#end
+
+## header view
+$this->load->view('header', $data);
+## this view
+$this->load->view('${page.id.toLowerCase()}', $data);
+## footer view
+$this->load->view('footer', $data);
 
 ?>
---
+++ trunk/src/main/resources/clickframes/php/autoscan/webapp/application/controllers/${appspec.id}/email_${email.id}.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+**	E-mail '${email.id}'
+**	${email.description}
+#foreach ($fact in $email.facts)**	- ${fact.description}
+#end
+*/
+
+// E-mail address of sender
+$emailFrom = '';
+
+// E-mail address of recipient
+$emailTo = '';
+
+// Specify all substitution variables for populating the e-mail text
+$emailVars = array(
+		'a' => 'b',
+		'c' => 'd'
+	);
+
+$result = $this->clickframes->sendMessage(
+		$emailFrom,
+		$emailTo,
+		$this->lang->line('email_${email.id}_subject'),
+		$this->lang->line('email_${email.id}_message'),
+		$emailVars
+	);
+
+if ($result !== TRUE) {
+	show_error("Unable to send email.\n\n" + $result);
+}
+
+?>
