c5ade789e5c16de037fd0b7b788cd1b29cca0e11
jonathan@abbett.org <jonathan@abbett.org@f1e69594-b2ab-11de-ae6a-cd50274c347e>
2010-08-02 21:51:13 +0000

    Added SQL generation.

---
+++ trunk/src/main/resources/clickframes/php/autoscan/src/main/sql/${appspec.id}.sql
@@ -0,0 +1,34 @@
+#macro (sqlType $type)#if ($type == 'TEXT')VARCHAR(255)#else${type}#end#end
+
+#foreach ($entity in $appspec.entities)
+CREATE TABLE IF NOT EXISTS `$entity.id` (
+#foreach ($property in $entity.properties)
+#if (!$property.multiple and $property.type != 'FILE')
+#if ($property.foreignEntityId != '')
+	`${property.id}` #sqlType($property.foreignEntity.primaryKey.type),
+#elseif ($property.persistent)	
+	`${property.id}` #sqlType($property.type)#if ($property.primaryKey and $property.type == 'INT') AUTO_INCREMENT#end,
+#end
+#end
+#end
+	PRIMARY KEY (`${entity.primaryKey.id}`) 
+);
+
+#foreach ($property in $entity.properties)
+#if ($property.multiple)
+CREATE TABLE IF NOT EXISTS `${entity.id}_${property.id}` (
+	`${entity.id}_${entity.primaryKey.id}` #sqlType($entity.primaryKey.type),
+#if ($property.foreignEntityId != '')
+	`${property.foreignEntity.id}_${property.foreignEntity.primaryKey.id}` #sqlType($property.foreignEntity.primaryKey.type)
+#else
+	`${property.id}` #sqlType($property.type)
+#end
+
+);
+
+#end
+#end
+
+#end
+
+-- clickframes::::clickframes
