c05028500d1841ec48d0f41d4af2fb84e25829ef
jonathan <jonathan@abbett.org>
2010-08-04 14:04:46 +0000

    Refactoring model, adding SQL generation for MySQL and SQLite

---
+++ src/main/resources/clickframes/php/autoscan/src/main/php/webapp/application/models/dto/${entity.name}DTO.class.php
@@ -0,0 +1,32 @@
+#set($dollarSign="$")
+<?php
+
+include_once('Abstract${entity.name}DTO.class.php');
+
+class ${entity.name}DTO extends Abstract${entity.name}DTO {
+    
+#foreach ($property in $entity.properties)
+    private ${dollarSign}${property.id};
+#end
+    
+#foreach ($property in $entity.properties)
+#if ($property.type.toUpperCase() == "BOOLEAN")
+    function is${property.name}() {
+#else
+    function get${property.name}() {
+#end
+        return $this->${property.id};
+    }
+
+#if ($property.persistent)
+    function set${property.name}(${dollarSign}${property.id}) {
+        $this->${property.id} = ${dollarSign}${property.id};
+    }
+
+#end
+#end
+    
+}
+
+/* clickframes::::clickframes */
+?>
